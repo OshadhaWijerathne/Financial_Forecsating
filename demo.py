cf0d9fbdbb30d9b885256a74bb100a008c1d10de
OshadhaWijerathne <oshadha.20@cse.mrt.ac.lk>
2024-03-07 14:19:53 +0530

    Update demo.py and add requirment.txt file

--- demo.py
+++ demo.py
@@ -4,27 +4,13 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import plotly.express as px
 import plotly.graph_objects as go
-
 import yfinance as yf
 from datetime import datetime
 import time
 
 current_date = datetime.now().strftime('%Y-%m-%d')
 
-# print execution time for this function
-
-def print_execution_time(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        execution_time = end_time - start_time
-        print(f"Execution time for {func.__name__}: {execution_time} seconds")
-        return result
-    return wrapper
-
 @st.cache_data
-@print_execution_time
 def download_data(stock_symbol, start_date, end_date=current_date):
     amzn = yf.Ticker(stock_symbol)
     amzn_hist = amzn.history(start=start_date, end=end_date)
@@ -47,14 +33,15 @@ def predict_stock_price(forecast_period, forecast_feature,df):
     return forecast , m
 
 def take_input():
-    stock_symbol = st.selectbox('Select Stock Symbol:', ('AMZN', 'GOOGL', 'AAPL', 'MSFT', 'TSLA'))
-    start_date = st.date_input('Select Start Date:', datetime(2021, 1, 1))
-    end_date = st.date_input('Select End Date:', datetime.now())
+    with st.sidebar:
+        stock_symbol = st.selectbox('Select Stock Symbol:', ('AMZN', 'GOOGL', 'AAPL', 'MSFT', 'TSLA'))
+        start_date = st.date_input('Select Start Date:', datetime(2021, 1, 1))
+        end_date = st.date_input('Select End Date:', datetime.now())
 
-    forecast_feature = st.selectbox('Select Forecasting Feature:', ('Close', 'Open', 'High', 'Low'))
-    forecast_period = st.number_input('Forecast Period (days):', min_value=1, max_value=365, value=30)
+        forecast_feature = st.selectbox('Select Forecasting Feature:', ('Close', 'Open', 'High', 'Low'))
+        forecast_period = st.number_input('Forecast Period (days):', min_value=1, max_value=365, value=30)
 
-    return stock_symbol, start_date, end_date, forecast_feature, forecast_period
+        return stock_symbol, start_date, end_date, forecast_feature, forecast_period
 
 def show_forecast_plots(forecast, m, forecast_feature, end_date):
         st.subheader('Interactive Forecast Plot')
@@ -83,7 +70,7 @@ def main():
     stock_symbol, start_date, end_date, forecast_feature, forecast_period = take_input()
 
     # Make predictions and plot
-    if st.button('Generate Forecast'):
+    if st.sidebar.button('Generate Forecast'):
         with st.spinner('Generating Forecast...'):
             df = download_data(stock_symbol, start_date, end_date)
             forecast, m = predict_stock_price(forecast_period, forecast_feature, df)
